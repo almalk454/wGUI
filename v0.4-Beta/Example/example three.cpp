295d18dc3a65cb41c5ebd038e6b8051bd6d27e92
Alex <almalk454@mail.ru>
2014-06-08 14:08:14 +0600

    Create example three.cpp

---
+++ v0.4-Beta/Example/example three.cpp
@@ -0,0 +1,20 @@
+#include "wGUI"
+
+LPTSTR szClassName = "App on wGUI";
+
+void onButtonClick(TButton* self)
+{
+  MessageBoxA(0, "Was clicked button!", szClassName, 0);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
+{
+  wgui.init(hInstance, szClassName);
+  TForm form1 = wgui.CreateForm("Form Caption", 500, 400);
+  TButton button1("Button");
+  button1.onClick = onButtonClick;
+  wgui.addGUI(&button1, &form1);
+  
+  
+  wGUI_MSGS;///(!) necessarily...
+}
