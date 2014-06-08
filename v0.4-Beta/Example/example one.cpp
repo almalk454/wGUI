fd7680929023d3a8fde843d5d23472658b123e32
Alex <almalk454@mail.ru>
2014-06-08 14:08:57 +0600

    Update example one.cpp

--- v0.4-Beta/Example/example one.cpp
+++ v0.4-Beta/Example/example one.cpp
@@ -4,7 +4,7 @@ LPTSTR szClassName = "App on wGUI";
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 {
-  wgui.ini(hInstance, szClassName);
+  wgui.init(hInstance, szClassName);
   TForm form1 = wgui.CreateForm("Form Caption", 500, 400);
   form1.Show();
   
